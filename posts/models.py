3b94f9b4d8b720275d65e33765b5c380b632becf
Mhlengi Ngubane <87973826+MhlengiNgubane@users.noreply.github.com>
2024-10-17 01:28:43 +0200

    update comments in posts

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ from django.db import models
 User = get_user_model()
 
 class Hashtag(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
